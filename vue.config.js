e88c069c59540fc2ce55ef8a4da6e5a4df8a1abd
Usnie <usnie@mail.ru>
2022-05-15 01:26:41 +0300

    new

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/click-counter/'
+  transpileDependencies: true//,
+  //publicPath: '/click-counter/'
 })
