903121659b700991fcdb2a4247576d103a8a7e4c
Usnie Abdulaeva <usnie.91@gmail.com>
2024-04-27 02:07:36 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ButtonCount />
+ </div>
+</template>
+<script>
+import ButtonCount from './components/ButtonCount.vue'
+export default {
+  name: 'App',
+  components: {
+    ButtonCount
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ButtonCount.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>Welcome to button count application</h3>
+    <p>Button count is: {{count}}</p>
+    <button id = "btn-increment" @click="onClickIncremet">Increment</button>
+    <button style="margin-left: 10px;" id = "btn-reset" @click="onClickReset">Reset </button>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    onClickIncremet () {
+      this.count += 1 // count increment
+    },
+    onClickReset () {
+      this.count = 0 // count reset
+    }
+  }
+}
+</script>
