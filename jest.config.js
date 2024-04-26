59fb4045b377e08495712d04a6cf22d0a2603fc0
Usnie Abdulaeva <usnie.91@gmail.com>
2024-04-27 02:15:26 +0300

    Expand jest.config.js file

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,15 @@
+Листинг 4. Код jest.config.js
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
-}
+    preset: '@vue/cli-plugin-unit-jest',
+    collectCoverage: true,
+    collectCoverageFrom: ['**/*.vue', "!**/node_modules/**"],
+    coverageReporters: ['lcov', 'text-summary'],
+    coverageThreshold: {
+      "global": {
+        "branches": 80,
+        "functions": 80,
+        "lines": 50,
+        "statements": -15
+      }
+    }
+  }
