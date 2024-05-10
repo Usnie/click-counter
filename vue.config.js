87ed4b508470d0f5275b37e047f46a6eeffa7a82
Usnie Abdulaeva <usnie.91@gmail.com>
2024-05-10 18:42:49 +0300

    Created main.yml

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/click-counter/'  //you have to add your repo name here 
 })
